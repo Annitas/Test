f6112bfaea4a0d43743b16c245dded191181ac9a
Annitas <39757257+Annitas@users.noreply.github.com>
2024-01-31 16:07:07 +0600

    Write product card description

--- Test/Test/ContentView.swift
+++ Test/Test/ContentView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        LazyVStack(alignment: .leading, spacing: 20.0) {
             Image("productCard")
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Добавка \"Липа\" к чаю 200 г")
-            Text("Испания, Риоха")
+                .font(.system(size: 26, weight: .bold, design: .rounded))
+            HStack {
+                Image("spanishFlag")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .clipShape(Circle())
+                Text("Испания, Риоха")
+            }
+            
+            Text("Описание")
+                .font(.system(size: 20, weight: .bold, design: .rounded))
+            Text("Флавоноиды липового цвета обладают противовоспалительным действием, способствуют укреплению стенки сосудов")
+            Text("Основные характеристики")
+                .font(.system(size: 20, weight: .bold, design: .rounded))
+            Text("Все характеристики")
+                .font(.system(size: 20, weight: .bold, design: .rounded))
+                .foregroundColor(.green)
+                
+            Text("Отзывы")
+                .font(.system(size: 23, weight: .bold, design: .rounded))
         }
         .padding()
     }
