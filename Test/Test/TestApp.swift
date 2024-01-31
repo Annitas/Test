aa4bd51655bd43b349fc94be86201d9dd51b8a7f
Annitas <39757257+Annitas@users.noreply.github.com>
2024-01-31 15:32:57 +0600

    Start project

---
+++ Test/Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Anita Stashevskaya on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
