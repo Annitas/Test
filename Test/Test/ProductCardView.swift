79bb6915650bff0ecb43d27481b0b589caa939af
Annitas <39757257+Annitas@users.noreply.github.com>
2024-02-01 18:54:22 +0600

    Add button

--- Test/Test/ProductCardView.swift
+++ Test/Test/ProductCardView.swift
@@ -27,6 +27,19 @@ struct ProductCardView: View {
                 Text("Флавоноиды липового цвета обладают противовоспалительным действием, способствуют укреплению стенки сосудов")
                 Text("Основные характеристики")
                     .font(.system(size: 20, weight: .bold, design: .rounded))
+                Text("Производство")
+                    .font(.system(size: 18, weight: .regular, design: .rounded))
+                Text("Энергетическая ценность, ккал/100г")
+                    .font(.system(size: 18, weight: .regular, design: .rounded))
+                Text("Жиры/100г")
+                    .font(.system(size: 18, weight: .regular, design: .rounded))
+                Text("Белки/100г")
+                    .font(.system(size: 18, weight: .regular, design: .rounded))
+                Text("Углеводы/100г")
+                    .font(.system(size: 18, weight: .regular, design: .rounded))
+                
+                
+                
                 Text("Все характеристики")
                     .font(.system(size: 20, weight: .bold, design: .rounded))
                     .foregroundColor(.green)
@@ -41,7 +54,25 @@ struct ProductCardView: View {
                         }
                     }
                 }
-                
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        // action
+                    }
+                    ) {
+                        Text("Оставить отзыв")
+                            .font(.system(size: 18, weight: .bold, design: .rounded))
+                            .padding(10)
+                            .frame(width: 300, height: 40, alignment: .center)
+                            .foregroundColor(.green)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(Color.green, lineWidth: 2)
+                            )
+                    }
+                    Spacer()
+                }
+
             }
             .padding()
         }
